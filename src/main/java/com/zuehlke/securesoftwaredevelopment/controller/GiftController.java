ae4cecf8cc6129fa422d80feabda9973d9303b61
alajdza <garfildk007@gmail.com>
2024-01-19 19:20:34 +0100

    endpoint protection and rendering based on permissions TODO: update_person

--- src/main/java/com/zuehlke/securesoftwaredevelopment/controller/GiftController.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/GiftController.java
@@ -5,6 +5,7 @@ import com.zuehlke.securesoftwaredevelopment.domain.*;
 import com.zuehlke.securesoftwaredevelopment.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -38,12 +39,14 @@ public class GiftController {
     }
 
     @GetMapping("/")
+    @PreAuthorize("hasAuthority('VIEW_GIFT_LIST')")
     public String showSearch(Model model) {
         model.addAttribute("gifts", giftRepository.getAll());
         return "gifts";
     }
 
     @GetMapping("/create-form")
+    @PreAuthorize("hasAuthority('CREATE_GIFT')")
     public String CreateForm(Model model) {
         model.addAttribute("tags", tagRepository.getAll());
         return "create-form";
@@ -56,6 +59,7 @@ public class GiftController {
     }
 
     @GetMapping("/gifts")
+    @PreAuthorize("hasAuthority('VIEW_GIFT_LIST')")
     public String showGift(@RequestParam(name = "id", required = false) String id, Model model, Authentication authentication) {
         if (id == null) {
             model.addAttribute("gifts", giftRepository.getAll());
@@ -91,6 +95,7 @@ public class GiftController {
     }
 
     @PostMapping("/gifts")
+    @PreAuthorize("hasAuthority('CREATE_GIFT')")
     public String createGift(NewGift newGift) throws SQLException {
         List<Tag> tagList = this.tagRepository.getAll();
         List<Tag> tagsToInsert = newGift.getTags().stream().map(tagId -> tagList.stream().filter(tag -> tag.getId() == tagId).findFirst().get()).collect(Collectors.toList());
@@ -99,6 +104,7 @@ public class GiftController {
     }
 
     @GetMapping("/buy-gift/{id}")
+    @PreAuthorize("hasAuthority('BUY_GIFT')")
     public String showBuyCar(
             @PathVariable("id") int id,
             @RequestParam(required = false) boolean addressError,
--- src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/PersonsController.java
@@ -9,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -34,6 +36,7 @@ public class PersonsController {
     }
 
     @GetMapping("/persons/{id}")
+    @PreAuthorize("hasAuthority('VIEW_PERSON')")
     public String person(@PathVariable int id, Model model, HttpSession session) {
         model.addAttribute("person", personRepository.get("" + id));
         String csrfToken = session.getAttribute("CSRF_TOKEN").toString();
@@ -43,6 +46,7 @@ public class PersonsController {
     }
 
     @GetMapping("/myprofile")
+    @PreAuthorize("hasAuthority('VIEW_MY_PROFILE')")
     public String self(Model model, Authentication authentication) {
         User user = (User) authentication.getPrincipal();
         model.addAttribute("person", personRepository.get("" + user.getId()));
@@ -50,6 +54,8 @@ public class PersonsController {
     }
 
     @DeleteMapping("/persons/{id}")
+    // ovu permisiju nemaju kupac i menadzer
+    @PreAuthorize("hasAuthority('UPDATE_PERSON')")
     public ResponseEntity<Void> person(@PathVariable int id) {
         personRepository.delete(id);
         userRepository.delete(id);
@@ -58,6 +64,7 @@ public class PersonsController {
     }
 
     @PostMapping("/update-person")
+    @PreAuthorize("hasAuthority('UPDATE_PERSON')")
     public String updatePerson(Person person, @RequestParam("csrf_token") String csrfToken, HttpSession session) throws AccessDeniedException {
         if(!session.getAttribute("CSRF_TOKEN").toString().equals(csrfToken)){
             throw new AccessDeniedException("Forbidden");
@@ -67,6 +74,7 @@ public class PersonsController {
     }
 
     @GetMapping("/persons")
+    @PreAuthorize("hasAuthority('VIEW_PERSONS_LIST')")
     public String persons(Model model) {
         model.addAttribute("persons", personRepository.getAll());
         return "persons";
